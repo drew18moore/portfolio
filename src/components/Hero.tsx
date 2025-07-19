075d772c19bf8e6e2e983cc32a1a8850115cb1f0
Drew <dcm752052@gmail.com>
2025-07-19 16:15:35 -0400

    Created Hero. Added Space Grotesk font.

---
+++ src/components/Hero.tsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <section className="text-center flex flex-col gap-4 items-center justify-center h-screen">
+      <div className="flex flex-col">
+				<h1 className="text-9xl font-bold bg-gradient-to-r from-cyan-400 to-blue-500 bg-clip-text text-transparent">Drew</h1>
+				<h1 className="text-9xl font-bold bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent">Moore</h1>
+			</div>
+      <p className="text-xl">Full-stack software engineer</p>
+			<span className="flex gap-4">
+				<button className="border border-white/90 px-4 py-2 rounded-md cursor-pointer">View my resume</button>
+				<button className="border border-white/90 px-4 py-2 rounded-md cursor-pointer">Read my blog</button>
+			</span>
+    </section>
+  );
+};
+
+export default Hero;
